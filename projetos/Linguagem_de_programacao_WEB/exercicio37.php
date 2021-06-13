16921e1ff60642d8956f654b71decfcafc171d3f
Luís Henrique <turaniluishenrique@gmail.com>
2021-06-13 15:03:22 -0300

    Atualizado conexões com o BD do novo servidor

--- projetos/Linguagem_de_programacao_WEB/exercicio37.php
+++ projetos/Linguagem_de_programacao_WEB/exercicio37.php
@@ -22,10 +22,10 @@
         <?php
 
             define("DSN", "mysql");
-            define("SERVIDOR", "sql312.epizy.com");
-            define("USUARIO", "epiz_26061509");
-            define("SENHA", "geyKgAixlnZF");
-            define("BANCODEDADOS", "epiz_26061509_teste");
+            define("SERVIDOR", "sql202.epizy.com");
+            define("USUARIO", "epiz_28871846");
+            define("SENHA", "a1L09N1lTjHB4DW");
+            define("BANCODEDADOS", "epiz_28871846_ativ_desaf");
             function conectar() {
 
                 $conn = new PDO(DSN.':host='.SERVIDOR.';dbname='.BANCODEDADOS,USUARIO,SENHA);//a conexão é estabelecida, recebendo as constantes como valor nos seus campos
--- projetos/programacao_para_internet/unidade06/AtividadeDesafiadora/controler/Conector.php
+++ projetos/programacao_para_internet/unidade06/AtividadeDesafiadora/controler/Conector.php
@@ -1,13 +1,14 @@
 <?php
 //constantes com os valores para a conexão
 define("DSN", "mysql");
-define("SERVIDOR", "sql312.epizy.com");
-define("USUARIO", "epiz_26061509");
-define("SENHA", "geyKgAixlnZF");
-define("BANCODEDADOS", "epiz_26061509_teste");
-function conectar() {
-    
-    $conn = new PDO(DSN.':host='.SERVIDOR.';dbname='.BANCODEDADOS,USUARIO,SENHA);//a conexão é estabelecida, recebendo as constantes como valor nos seus campos
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//seta atributos para tratar os erros
-    return $conn;//retorna a conexão, retornando todo o Objeto da conexão
+define("SERVIDOR", "sql202.epizy.com");
+define("USUARIO", "epiz_28871846");
+define("SENHA", "a1L09N1lTjHB4DW");
+define("BANCODEDADOS", "epiz_28871846_ativ_desaf");
+function conectar()
+{
+
+    $conn = new PDO(DSN . ':host=' . SERVIDOR . ';dbname=' . BANCODEDADOS, USUARIO, SENHA); //a conexão é estabelecida, recebendo as constantes como valor nos seus campos
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //seta atributos para tratar os erros
+    return $conn; //retorna a conexão, retornando todo o Objeto da conexão
 }
